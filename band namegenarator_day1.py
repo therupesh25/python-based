d1ee7fa6051337ab5829ec8c7192c109834484f0
AITHA RUPESH <130269945+therupesh25@users.noreply.github.com>
2024-05-28 19:24:42 +0530

    Update band namegenarator_day1.py

---
+++ band namegenarator_day1.py
@@ -0,0 +1,5 @@
+print("Welcome to the Bandname Generator.")
+name = input("what's name of the city you grow up in ?")
+pet = input("What's your pet's name ?")
+print("Your Bandname could be",name+pet)
+
